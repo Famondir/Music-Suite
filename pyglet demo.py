c1b4e3a8c38015c336bd7460b7a8b4ec66ef79b5
Famondir <schaefersd@gmail.com>
2023-10-21 19:59:44 +0200

    Verified that multiple sounds can be played simultanious.

--- pyglet demo.py
+++ pyglet demo.py
@@ -1,4 +1,5 @@
 import pyglet
+from pyglet.window import key
 
 window = pyglet.window.Window()
 
@@ -8,9 +9,34 @@ label = pyglet.text.Label('Hello, world',
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
 
+player = pyglet.media.Player()
+player.loop = True
+player2 = pyglet.media.Player()
+
+
 @window.event
 def on_draw():
     window.clear()
     label.draw()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print(f'The key {symbol} was pressed')
+    if symbol == key.A:
+        print('The "A" key was pressed.')
+        music = pyglet.media.synthesis.Sine(3.0, frequency=440, sample_rate=44800)
+        player.queue(music)
+        player.play()
+    if symbol == key.B:
+        print('The "B" key was pressed.')
+        music = pyglet.media.synthesis.Sine(3.0, frequency=880, sample_rate=44800)
+        player2.queue(music)
+        player2.play()
+
+@window.event
+def on_key_release(symbol, modifiers):
+    print("Stop")
+    if symbol == key.A:
+        player.pause()
 
 pyglet.app.run()

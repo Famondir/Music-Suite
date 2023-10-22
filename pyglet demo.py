0931f0bc4e30ea73171d467635f12997b315e976
Famondir <schaefersd@gmail.com>
2023-10-22 21:38:03 +0200

    Added first part of keymapping. Created TonePlayer class.

--- pyglet demo.py
+++ pyglet demo.py
@@ -9,9 +9,39 @@ label = pyglet.text.Label('Hello, world',
                           x=window.width//2, y=window.height//2,
                           anchor_x='center', anchor_y='center')
 
-player = pyglet.media.Player()
-player2 = pyglet.media.Player()
+# player = pyglet.media.Player()
+# player2 = pyglet.media.Player()
+playerlist = []
 
+keymapping = {
+    944892805120: 53, 65505: 53, 
+    65289: 54, 
+    65509: 55, 
+    60: 56, 
+    49: 56, 
+    113: 57, 
+    97: 58, 
+    121: 59, 50: 59, 
+    119: 60, 
+    115: 61, 
+    120: 62, 51: 62
+    }
+
+class TonePlayer:
+    def __init__(self, midiTone):
+        self.midiTone = midiTone
+        self.player = pyglet.media.Player()
+        self.music = pyglet.media.load("wav/Accordion 0"+str(self.midiTone)+".wav", streaming=False)
+
+    def playTone(self):
+        self.player.queue(self.music)
+        self.player.play()
+
+    def stopTone(self):
+        self.player.pause()
+
+    def getmidiTone(self):
+        return self.midiTone    
 
 @window.event
 def on_draw():
@@ -21,9 +51,13 @@ def on_draw():
 @window.event
 def on_key_press(symbol, modifiers):
     print(f'The key {symbol} was pressed')
-    if symbol == key.A:
+    if symbol in keymapping:
+        player = TonePlayer(keymapping[symbol])
+        playerlist.append(player)
+        player.playTone()
+"""     if symbol == key.A:
         print('The "A" key was pressed.')
-        music = pyglet.media.load("wav/Accordion 072.wav", streaming=False)
+        music = pyglet.media.load("wav/Accordion 058.wav", streaming=False)
         player.queue(music)
         player.play()
     if symbol == key.B:
@@ -31,12 +65,22 @@ def on_key_press(symbol, modifiers):
         music = pyglet.media.load("wav/Accordion 076.wav", streaming=False)
         # music = pyglet.media.synthesis.Sine(3.0, frequency=440, sample_rate=44800)
         player2.queue(music)
-        player2.play()
+        player2.play() """
 
 @window.event
 def on_key_release(symbol, modifiers):
-    print("Stop")
-    if symbol == key.A:
+    print(f'The key {symbol} was released')
+    if symbol in keymapping:
+        for player in playerlist:
+            if keymapping[symbol] == player.getmidiTone():
+                player.stopTone()
+                del player
+"""     if symbol == key.A:
+        print("Stop")
         player.pause()
+    if symbol == key.B:
+        print("Stop")
+        player2.pause() """
+        
 
 pyglet.app.run()
